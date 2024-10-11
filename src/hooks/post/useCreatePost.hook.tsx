fcea92fc2f5d1bd9492d58a32f9672925f088e05
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-11 10:46:58 +0600

    create post service and useCreatePost hook

---
+++ src/hooks/post/useCreatePost.hook.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createPost } from "@/services/post/post.services";
+import { getQueryClient } from "@/utils/getQueryClient";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export const useCreatePost = () => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationKey: ["CREATE_POST"],
+    mutationFn: async (postData: any) => await createPost(postData),
+    onSuccess: () => {
+      queryClient.cancelQueries({ queryKey: ["POSTS"] });
+      toast.success("Post Created Successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message)
+      console.log(error.message, 'error from hook');
+    }
+  });
+};
---
+++ src/services/post/post.services.ts
@@ -0,0 +1,163 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server"
+
+import axiosInstance from "@/config/axiosInstance";
+import { IErrorResponse, IPost, ISuccessfulResponse } from "@/types"
+// NOTE: createPost services
+export  const createPost = async (postData:any)=>{
+  try {
+    const response = await axiosInstance.post<ISuccessfulResponse<IPost>>("/posts/create-post",postData);
+    if(response.data.success){
+      return response.data.data
+    }
+    else {
+      console.log('error creating post',response.data.message);
+      return null
+    }
+  } catch (error:any) {
+  
+    if (error.response) {
+      const errorData: IErrorResponse = error.response.data;
+      console.error("Error creating post:", errorData.message, errorData.errorMessages);
+    } else {
+      console.error("An unexpected error occurred:", error.message);
+    }
+    return null; 
+  }
+  
+}
+
+// NOTE: fetch all posts services
+interface PostsData {
+  result: IPost[]
+  totalPosts: number
+}
+
+const POSTS_PER_PAGE = 2;
+
+export const fetchAllPosts = async ({ pageParam = 1, category }: { pageParam?: number; category?: string }): Promise<ISuccessfulResponse<PostsData>> => {
+  const { data } = await axiosInstance.get<ISuccessfulResponse<PostsData>>('/posts', {
+    params: {
+      page: pageParam,
+      limit: POSTS_PER_PAGE,
+      category: category || undefined
+    },
+  });
+  return data;
+};
+
+// NOTE: Like a post
+
+export const likeAPost = async(postId:string)=>{
+  try {
+    const response = await axiosInstance.patch<ISuccessfulResponse<IPost>>(
+      `/posts/likes/${postId}`);
+      if(response.data.success){
+        return response.data.data
+      }else {
+        console.error("Failed to like a post:", response.data.message);
+      return null; 
+      }
+  } catch (error:any) {
+    console.log(error);
+     if (error.response) {
+      const errorData: IErrorResponse = error.response.data;
+      console.error("Error creating post:", errorData.message, errorData.errorMessages);
+    } else {
+      console.error("An unexpected error occurred:", error.message);
+    }
+    return null; 
+  }
+  
+}
+
+// NOTE: dislike a post
+export const dislikeAPost = async(postId:string)=>{
+  try {
+    const response = await axiosInstance.patch<ISuccessfulResponse<IPost>>(
+      `/posts/dislikes/${postId}`);
+      if(response.data.success){
+        return response.data.data
+      }else {
+        console.error("Failed to dislike a post:", response.data.message);
+      return null; 
+      }
+  } catch (error:any) {
+     if (error.response) {
+      const errorData: IErrorResponse = error.response.data;
+      console.error("Error creating post:", errorData.message, errorData.errorMessages);
+    } else {
+      console.error("An unexpected error occurred:", error.message);
+    }
+    return null; 
+  }
+  
+}
+
+
+export const getSinglePost = async (postId:string)=>{
+  try{
+   const response = await axiosInstance.get<ISuccessfulResponse<IPost>>(
+      `/posts/${postId}`);
+
+    if (response.data.success) {
+      return response.data.data; 
+    } else {
+      console.error("Failed to create post:", response.data.message);
+      return null; 
+    }
+  } catch (error: any) {
+    if (error.response) {
+      const errorData: IErrorResponse = error.response.data;
+      console.error("Error creating post:", errorData.message, errorData.errorMessages);
+    } else {
+      console.error("An unexpected error occurred:", error.message);
+    }
+    return null; 
+  }
+}
+export const editASinglePost = async (payload:any)=>{
+  try{
+    const {postId,...body} = payload;
+   const response = await axiosInstance.patch<ISuccessfulResponse<IPost>>(
+      `/posts/${postId}`,body);
+    
+    if (response.data.success) {
+      return response.data.data; 
+    } else {
+      console.error("Failed to edit post:", response.data.message);
+      return null; 
+    }
+  } catch (error: any) {
+    if (error.response) {
+      const errorData: IErrorResponse = error.response.data;
+      console.error("Error editing post:", errorData.message, errorData.errorMessages);
+    } else {
+      console.error("An unexpected error occurred:", error.message);
+    }
+    return null; 
+  }
+}
+export const deleteASinglePost = async (postId:string)=>{
+  try{
+   const response = await axiosInstance.delete<ISuccessfulResponse<IPost>>(
+      `/posts/${postId}`);
+    
+    if (response.data.success) {
+      return response.data.data; 
+    } else {
+      console.error("Failed to delete post:", response.data.message);
+      return null; 
+    }
+  } catch (error: any) {
+    if (error.response) {
+      const errorData: IErrorResponse = error.response.data;
+      console.error("Error deleting post:", errorData.message, errorData.errorMessages);
+    } else {
+      console.error("An unexpected error occurred:", error.message);
+    }
+    return null; 
+  }
+}
+
+
