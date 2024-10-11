0e04a45a22c9d25bcd4ac06156571b31b63d136b
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-11 12:27:48 +0600

    queryoptions for hydration

---
+++ src/queryOptions/infiniteQueryOptionsForPost.tsx
@@ -0,0 +1,23 @@
+import { fetchAllPosts } from "@/services/post/post.services";
+import { infiniteQueryOptions } from "@tanstack/react-query";
+export const postOptions = infiniteQueryOptions({
+  queryKey: ["POSTS"],
+  queryFn: ({ pageParam = 1 }) => fetchAllPosts({ pageParam }),
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages) => {
+    const loadedPosts = allPages.reduce((total, page) => total + page.data.result.length, 0);
+    return loadedPosts < lastPage.data.totalPosts ? allPages.length + 1 : undefined;
+  },
+  refetchInterval: 3 * 60 * 1000
+})
+export const postOptionsForStoryOrTip = (category?: string) => infiniteQueryOptions({
+  queryKey: ["POSTS", category],
+  queryFn: ({ pageParam = 1 }) => fetchAllPosts({ pageParam, category }),
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages) => {
+    console.log(lastPage.data.totalPosts);
+    const loadedPosts = allPages.reduce((total, page) => total + page.data.result.length, 0);
+    return loadedPosts < lastPage.data.totalPosts ? allPages.length + 1 : undefined;
+  },
+  refetchInterval: 3 * 60 * 1000
+})
