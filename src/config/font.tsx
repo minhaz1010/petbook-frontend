3ced94d217bcfb3d8a9b995cb81e0ddaeb692631
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-11 10:27:37 +0600

    configure the font

---
+++ src/config/font.tsx
@@ -0,0 +1,3 @@
+import { JetBrains_Mono, Protest_Strike } from "next/font/google"
+export const jetbrains = JetBrains_Mono({ subsets: ["latin"] })
+export const protest_strike = Protest_Strike({ subsets: ["latin"], weight: "400" })
