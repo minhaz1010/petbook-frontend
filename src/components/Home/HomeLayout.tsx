52d041488f085ffa6aaa76e1b284f4bc64fb1f71
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-13 00:40:13 +0600

    changes in Homelayout

--- src/components/Home/HomeLayout.tsx
+++ src/components/Home/HomeLayout.tsx
@@ -12,19 +12,19 @@ const HomeLayout = ({ children }: FeedLayoutProps) => {
     <div className="container mx-auto px-4 lg:px-0">
       <div className="flex flex-col lg:flex-row">
         {/* Left sidebar */}
-        <div className=" lg:sticky lg:top-0 mt-11 lg:h-screen lg:overflow-y-auto mb-4 lg:mb-0">
+        {/* <div className=" lg:sticky lg:top-0 mt-11 lg:h-screen lg:overflow-y-auto mb-4 lg:mb-0">
+          {childrenArray[0]}
+        </div> */}
+
+        {/* Middle content */}
+        <div className="lg:size-[70%] container w-full mx-auto mb-4 lg:mb-0">
           {childrenArray[0]}
         </div>
 
-        {/* Middle content */}
-        <div className="lg:w-1/2 w-full mx-auto mb-4 lg:mb-0">
-          {childrenArray[1]}
-        </div>
-
         {/* Right sidebar */}
-        <div className=" lg:sticky mt-11 lg:top-0 lg:h-screen lg:overflow-y-auto">
+        {/* <div className=" lg:sticky mt-11 lg:top-0 lg:h-screen lg:overflow-y-auto">
           {childrenArray[2]}
-        </div>
+        </div> */}
       </div>
     </div>
   );
