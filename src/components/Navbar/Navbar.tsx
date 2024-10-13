1ac7481716a239b1668ae16e23d1743b3a651c3b
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-13 17:28:17 +0600

    changes in navbar and add some link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,26 @@
-"use client"
-import { useState } from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { Menu, X, Home, BookOpen, PenTool, User, LogIn, LogOut } from 'lucide-react';
-import { UserButton, useAuth } from '@clerk/nextjs';
-import petImage from "../../../public/pet-dress.png"
-import Image from 'next/image';
-import { protest_strike } from '@/config/font';
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  Menu,
+  X,
+  Home,
+  BookOpen,
+  PenTool,
+  User,
+  LogIn,
+  LogOut,
+  DollarSign,
+  Shield,
+  Store,
+} from "lucide-react";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import petImage from "../../../public/pet-dress.png";
+import Image from "next/image";
+import { oswald } from "@/config/font";
+import { getSessionMetadata } from "@/services/user/user.services";
+import Loading from "../Shared/Loading";
 
 interface NavLinkProps {
   href: string;
@@ -16,14 +30,20 @@ interface NavLinkProps {
   onClick?: () => void;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ href, children, className = '', icon, onClick }) => {
+const NavLink: React.FC<NavLinkProps> = ({
+  href,
+  children,
+  className = "",
+  icon,
+  onClick,
+}) => {
   const pathname = usePathname();
   const isActive = pathname === href;
 
   return (
     <Link
       href={href}
-      className={`${className} ${isActive ? 'text-blue-600' : 'text-gray-300'
+      className={`${className} ${isActive ? "text-blue-600" : "text-gray-300"
         } hover:text-blue-500 transition-all duration-300 relative group flex items-center`}
       onClick={onClick}
     >
@@ -36,13 +56,32 @@ const NavLink: React.FC<NavLinkProps> = ({ href, children, className = '', icon,
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      try {
+        const role = await getSessionMetadata();
+        setRole(role as string);
+      } catch (error) {
+        console.error("Error fetching session metadata:", error);
+      }
+    };
+
+    fetchRole();
+  }, []);
+
+  if (!isLoaded) {
+    return <Loading />
+  }
+
 
   const toggleMenu = () => setIsOpen(!isOpen);
   const closeMenu = () => setIsOpen(false);
 
   return (
-    <nav className={` sticky top-0 z-50 ${protest_strike.className}`}>
+    <nav className={` sticky top-0 z-50 ${oswald.className}`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
@@ -60,22 +99,66 @@ const Navbar: React.FC = () => {
             </Link>
           </div>
 
-          <div className="hidden md:flex md:items-center md:space-x-8">
-            <NavLink href="/" className='text-xl transform hover:-translate-y-0.5 transition-transform duration-200' icon={<Home size={20} />}>
+          <div className="hidden lg:flex lg:items-center md:space-x-8">
+            <NavLink
+              href="/"
+              className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+              icon={<Home size={20} />}
+            >
               Home
             </NavLink>
-            <NavLink href="/story" className='text-xl transform hover:-translate-y-0.5 transition-transform duration-200' icon={<BookOpen size={20} />}>
+            <NavLink
+              href="/story"
+              className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+              icon={<BookOpen size={20} />}
+            >
               Story
             </NavLink>
-            <NavLink href="/tips" className='text-xl transform hover:-translate-y-0.5 transition-transform duration-200' icon={<BookOpen size={20} />}>
+            <NavLink
+              href="/tips"
+              className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+              icon={<BookOpen size={20} />}
+            >
               Tips
             </NavLink>
-            <NavLink href="/write" className='text-xl transform hover:-translate-y-0.5 transition-transform duration-200' icon={<PenTool size={20} />}>
+            <NavLink
+              href="/about"
+              className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+              icon={<Store size={20} />}
+            >
+              About
+            </NavLink>
+            <NavLink
+              href="/write"
+              className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+              icon={<PenTool size={20} />}
+            >
               Write
             </NavLink>
+            {/* New Pricing Link */}
+            <NavLink
+              href="/pricing"
+              className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+              icon={<DollarSign size={20} />}
+            >
+              Pricing
+            </NavLink>
+            {role === "ADMIN" && (
+              <NavLink
+                href="/admin/all-posts"
+                className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+                icon={<Shield size={20} />}
+              >
+                Admin
+              </NavLink>
+            )}
             {isSignedIn ? (
               <div className="flex items-center space-x-4">
-                <NavLink href="/profile" className='text-xl transform hover:-translate-y-0.5 transition-transform duration-200' icon={<User size={20} />}>
+                <NavLink
+                  href="/profile"
+                  className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+                  icon={<User size={20} />}
+                >
                   Profile
                 </NavLink>
                 <div className="transform hover:scale-110 transition-transform duration-200">
@@ -86,12 +169,16 @@ const Navbar: React.FC = () => {
               <div className="flex items-center space-x-4">
                 <NavLink
                   href="/sign-up"
-                  className='text-xl transform hover:-translate-y-0.5 transition-transform duration-200'
+                  className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
                   icon={<LogOut size={20} />}
                 >
                   Sign Up
                 </NavLink>
-                <NavLink href="/sign-in" className='text-xl transform hover:-translate-y-0.5 transition-transform duration-200' icon={<LogIn size={20} />}>
+                <NavLink
+                  href="/sign-in"
+                  className="text-xl transform hover:-translate-y-0.5 transition-transform duration-200"
+                  icon={<LogIn size={20} />}
+                >
                   Sign In
                 </NavLink>
               </div>
@@ -99,16 +186,22 @@ const Navbar: React.FC = () => {
           </div>
 
           {/* Mobile menu button */}
-          <div className="flex items-center md:hidden">
+          <div className="flex items-center lg:hidden">
             <button
               onClick={toggleMenu}
               className="text-gray-600 hover:text-gray-900 focus:outline-none transform hover:scale-110 transition-transform duration-200"
               aria-label="Toggle menu"
             >
               {isOpen ? (
-                <X size={24} className="transform rotate-0 transition-transform duration-300" />
+                <X
+                  size={24}
+                  className="transform rotate-0 transition-transform duration-300"
+                />
               ) : (
-                <Menu size={24} className="transform rotate-0 transition-transform duration-300" />
+                <Menu
+                  size={24}
+                  className="transform rotate-0 transition-transform duration-300"
+                />
               )}
             </button>
           </div>
@@ -117,9 +210,9 @@ const Navbar: React.FC = () => {
 
       {/* Mobile Navigation */}
       <div
-        className={`md:hidden transform transition-all duration-300 ease-in-out ${isOpen
-          ? 'opacity-100 translate-y-0'
-          : 'opacity-0 -translate-y-full pointer-events-none'
+        className={`lg:hidden transform transition-all duration-300 ease-in-out ${isOpen
+          ? "opacity-100 translate-y-0"
+          : "opacity-0 -translate-y-full pointer-events-none"
           }`}
       >
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-white shadow-lg rounded-b-lg">
@@ -147,6 +240,41 @@ const Navbar: React.FC = () => {
           >
             Tips
           </NavLink>
+          <NavLink
+            href="/about"
+            className="block px-3 py-2 rounded-md text-xl font-medium transform hover:translate-x-2 transition-transform duration-200"
+            icon={<Store size={20} />}
+            onClick={closeMenu}
+          >
+            About
+          </NavLink>
+          <NavLink
+            href="/write"
+            className="block px-3 py-2 rounded-md text-xl font-medium transform hover:translate-x-2 transition-transform duration-200"
+            icon={<BookOpen size={20} />}
+            onClick={closeMenu}
+          >
+            write
+          </NavLink>
+
+          <NavLink
+            href="/pricing"
+            className="block px-3 py-2 rounded-md text-xl font-medium transform hover:translate-x-2 transition-transform duration-200"
+            icon={<DollarSign size={20} />}
+            onClick={closeMenu}
+          >
+            Pricing
+          </NavLink>
+          {role === "ADMIN" && (
+            <NavLink
+              href="/admin/all-posts"
+              className="block px-3 py-2 rounded-md text-xl font-medium transform hover:translate-x-2 transition-transform duration-200"
+              icon={<Shield size={20} />}
+              onClick={closeMenu}
+            >
+              Admin
+            </NavLink>
+          )}
           {isSignedIn ? (
             <div className="space-y-1">
               <NavLink
