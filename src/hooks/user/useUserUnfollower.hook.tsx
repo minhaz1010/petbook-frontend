baf74a4f6d753143cfe0e4ad3d80cb2e4ecbdc93
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-13 19:36:03 +0600

    add toast

--- src/hooks/user/useUserUnfollower.hook.tsx
+++ src/hooks/user/useUserUnfollower.hook.tsx
@@ -1,6 +1,7 @@
 import { unfollowAUser } from "@/services/user/user.services"
 import { getQueryClient } from "@/utils/getQueryClient"
 import { useMutation } from "@tanstack/react-query"
+import { toast } from "sonner";
 
 export const useUserUnfollow = () => {
   const queryClient = getQueryClient();
@@ -8,7 +9,8 @@ export const useUserUnfollow = () => {
     mutationKey: ["UNFOLLOW_A_USER"],
     mutationFn: async (followerId: string) => await unfollowAUser(followerId),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["POSTS"] })
+      toast.success("You unfollowed the pet lover")
+      queryClient.invalidateQueries({ queryKey: ["POSTS"] });
     }
   })
 }
