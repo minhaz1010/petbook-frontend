032305590c84754b0e91ace202194d975a96296b
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-13 17:13:02 +0600

    add adminRoute to the clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,28 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-const isProtectedRoute = createRouteMatcher(['/write','/profile'])
+const isProtectedRoute = createRouteMatcher(['/write', '/profile(.*)',]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
+const isAdminRoute = createRouteMatcher(['/admin(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+
+  if (isProtectedRoute(req)) {
+      auth().protect();
+  }
+  if(isAdminRoute(req) && auth().sessionClaims?.metadata.role!=="ADMIN"){
+     const url = new URL('/', req.url)
+    return NextResponse.redirect(url)
+  }
+  
+});
+
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    // Always run for API routes,
     '/(api|trpc)(.*)',
   ],
-}
+};
