9af067fb43997de43e9ac9457faab4111a6f3891
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-13 19:36:12 +0600

    add toast

--- src/hooks/user/useUserFollower.hook.tsx
+++ src/hooks/user/useUserFollower.hook.tsx
@@ -1,6 +1,7 @@
 import { followAUser } from "@/services/user/user.services"
 import { getQueryClient } from "@/utils/getQueryClient"
 import { useMutation } from "@tanstack/react-query"
+import { toast } from "sonner";
 
 
 export const useUserFollower = () => {
@@ -9,6 +10,7 @@ export const useUserFollower = () => {
     mutationKey: ["FOLLOW_A_USER"],
     mutationFn: async (followerId: string) => await followAUser(followerId),
     onSuccess: () => {
+      toast.success("You followed the pet lover")
       queryClient.invalidateQueries({ queryKey: ['POSTS'] });
     }
   })
