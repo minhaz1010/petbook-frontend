daf34c772eb6dd0c59c9f89465e78fbf06a8c0ca
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-13 17:21:46 +0600

    changes in  home page

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,5 +1,3 @@
-import FollowersList from '@/components/Home/FollowerList';
-import FollowingsList from '@/components/Home/FollowingList';
 import HomeLayout from '@/components/Home/HomeLayout';
 import Post from '@/components/Home/Post';
 import CardLoader from '@/components/Shared/CardLoader';
@@ -13,16 +11,10 @@ const CommonPage = () => {
   void queryClient.prefetchInfiniteQuery(postOptions);
   return (
     <HomeLayout>
-      <Suspense fallback={<div>Loading followers...</div>}>
-        <FollowersList />
-      </Suspense>
       <Suspense fallback={<CardLoader />}>
         <HydrationBoundary state={dehydrate(queryClient)}>
           <Post />
         </HydrationBoundary>
-      </Suspense>
-      <Suspense fallback={<div>Loading following...</div>}>
-        <FollowingsList />
       </Suspense>
     </HomeLayout>
   )
