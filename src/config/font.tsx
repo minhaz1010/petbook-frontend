371f511a89c519761d46c1cf126e20b26821dba8
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-13 17:27:47 +0600

    add shadcn components and change in font config

--- src/config/font.tsx
+++ src/config/font.tsx
@@ -1,3 +1,3 @@
-import { JetBrains_Mono, Protest_Strike } from "next/font/google"
+import { JetBrains_Mono, Oswald } from "next/font/google"
 export const jetbrains = JetBrains_Mono({ subsets: ["latin"] })
-export const protest_strike = Protest_Strike({ subsets: ["latin"], weight: "400" })
+export const oswald = Oswald({ subsets: ["latin"] });
