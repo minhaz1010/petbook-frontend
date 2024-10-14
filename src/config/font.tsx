35064ab8ad92d8c75d27782b0ddb25fad68b7d0f
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-15 01:01:43 +0600

    add a font

--- src/config/font.tsx
+++ src/config/font.tsx
@@ -1,3 +1,4 @@
-import { JetBrains_Mono, Oswald } from "next/font/google"
+import { JetBrains_Mono, Oswald, Nunito_Sans, } from "next/font/google"
 export const jetbrains = JetBrains_Mono({ subsets: ["latin"] })
 export const oswald = Oswald({ subsets: ["latin"] });
+export const nunito = Nunito_Sans({ subsets: ["latin"] })
